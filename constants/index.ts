c26711cdc081e83b545ebd8f7e01054582edf66d
well-balanced <iam.woosik.kim@gmail.com>
2022-02-19 15:40:10 +0900

    :construction: Update constants

--- constants/index.ts
+++ constants/index.ts
@@ -2,11 +2,16 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export const WEBHOOK_URL: string = process.env.WEBHOOK_URL
-
 export const GITHUB_TOKEN: string = process.env.GITHUB_TOKEN
 
-const users = process.env.USERS.split(',')
-const names = process.env.NAMES.split(',')
+/**
+ * e.g.
+ * users = well-balanced,iam-woosik.kim
+ *  names = Woody Kim,Woosik Kim
+ * USER_NAME_MAP = { 'well-balanced': 'Woody Kim', 'iam-woosik-kim': 'Woosik Kim' }
+ */
+const users = process.env.USERS.split(',') || []
+const names = process.env.NAMES.split(',') || []
 export const USER_NAME_MAP = Array.from({ length: users.length }).reduce(
   (prev: any, _, idx) => ({ ...prev, [users[idx]]: names[idx] }),
   {},
