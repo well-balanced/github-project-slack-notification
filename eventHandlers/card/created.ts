1389ee50ab252faf4a0c0dffbfd43895c474a8e7
well-balanced <iam.woosik.kim@gmail.com>
2022-02-19 15:40:39 +0900

    :package: Add EventHandler

--- eventHandlers/card/created.ts
+++ eventHandlers/card/created.ts
@@ -1,8 +1,13 @@
+import { USER_NAME_MAP } from '../../constants'
 import { EH, NewCardEventPayload } from 'types'
 import { sendWebhook } from 'utils'
 
-export const cardCreatedEventehHandler: EH<NewCardEventPayload> = async (
+export const cardCreatedEventHandler: EH<NewCardEventPayload> = async (
   payload,
 ) => {
-  await sendWebhook('cardCreated')
+  const username = payload.sender.login
+  const name = USER_NAME_MAP[username] || '알 수 없음'
+  const task = payload.project_card.note
+  const message = ` *[${name}]* 님이 *[${task}]* 카드를 추가하셨습니다. \n\n<https://github.com/orgs/sixshop/projects/1|*칸반 보러가기*>`
+  await sendWebhook(message)
 }
