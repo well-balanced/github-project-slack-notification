bd3fb0dccb1153ade24d53bc5dae44917d33a101
well-balanced <iam.woosik.kim@gmail.com>
2022-02-19 15:41:04 +0900

    :package: Add Event Handler

--- eventHandlers/card/index.ts
+++ eventHandlers/card/index.ts
@@ -1,14 +1,14 @@
 import { UnsupportedEventException } from 'exceptions'
 import { CardAction, CardEventPayload } from 'types'
-import { cardCreatedEventehHandler } from './created'
-import { cardMovedEventpehHandler } from './moved'
+import { cardCreatedEventHandler } from './created'
+import { cardMovedEventHandler } from './moved'
 
 export function getCardEventHandler(payload: CardEventPayload) {
   switch (payload.action) {
     case CardAction.Created:
-      return cardCreatedEventehHandler
+      return cardCreatedEventHandler
     case CardAction.Moved:
-      return cardMovedEventpehHandler
+      return cardMovedEventHandler
     default:
       throw new UnsupportedEventException(
         `${payload?.action} is an unsupported card event`,
--- eventHandlers/card/moved.ts
+++ eventHandlers/card/moved.ts
@@ -3,12 +3,13 @@ import { EH, CardMovedEventPayload } from 'types'
 import { callGithubApi, sendWebhook } from 'utils'
 import { Column, Issue } from 'types'
 
-export const cardMovedEventpehHandler: EH<CardMovedEventPayload> = async (
+export const cardMovedEventHandler: EH<CardMovedEventPayload> = async (
   payload,
 ) => {
   const username = payload.sender.login
-  const name = USER_NAME_MAP[username]
+  const name = USER_NAME_MAP[username] || '알 수 없음'
   let title = payload.project_card.note
+
   if (!title) {
     const { data } = await callGithubApi<Issue>({
       url: payload.project_card.content_url,
@@ -20,14 +21,17 @@ export const cardMovedEventpehHandler: EH<CardMovedEventPayload> = async (
   const [currentColumnId, ...rest] = payload.project_card.column_url
     .split('/')
     .reverse()
+
+  if (prevColumnId === currentColumnId) {
+    return
+  }
+
   const columnUrl = rest.reverse().join('/')
   const [{ data: prevColumn }, { data: currentColumn }] = await Promise.all([
     callGithubApi<Column>({ url: `${columnUrl}/${prevColumnId}` }),
     callGithubApi<Column>({ url: `${columnUrl}/${currentColumnId}` }),
   ])
 
-  const messageFormat = ` *[${name}]* 님이 *[${title}]* 카드를 *[${prevColumn.name}]* ➡️ *[${currentColumn.name}]* 로 옮기셨습니다. \n\n<https://github.com/orgs/sixshop/projects/1|*칸반 보러가기*>`
-  if (prevColumnId !== currentColumnId) {
-    await sendWebhook(messageFormat)
-  }
+  const message = ` *[${name}]* 님이 *[${title}]* 카드를 *[${prevColumn.name}]* ➡️ *[${currentColumn.name}]* 로 옮기셨습니다. \n\n<https://github.com/orgs/sixshop/projects/1|*칸반 보러가기*>`
+  await sendWebhook(message)
 }
